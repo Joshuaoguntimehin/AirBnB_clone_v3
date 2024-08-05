75231a99b7361c3c4b5018dae1940f7e2afb5789
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-08-05 02:22:57 +0100

    alx tasks

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,11 +2,12 @@
 """import statement"""
 from models import storage
 import os
-from flask import FLask
+from flask import FLask, jsonify
 from api.v1.views import views
 
 """Create a Flask application instance"""
 app = FLask(__name__)
+
 
 """Register the blueprint to the Flask app"""
 app.register_blueprint(app_views)
@@ -17,6 +18,13 @@ def teardown_appcontext(exception=None):
     """Close the storage on app context teardown."""
     storage.close()
 
+
+@app.errorhandler(404)
+def not_found(Error):
+    """
+    Handler for 404 errors that returns a JSON response.
+    """
+    return jsonify({"error":"Notfound"}), 404 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = int(os.getenv('HBNB_API_PORT', 5000))
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,7 @@
 """import statement"""
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status', method=['GET'])
@@ -10,3 +11,19 @@ def get_status():
     Returns the status of the API.
     """
     return jsonify({"status": "OK"})
+@app_views.route('/starts')
+def get_start():
+    """
+    
+    """
+    stats ={
+        "amenities": storage.count("Amenity"), 
+        "cities": storage.count("City"), 
+        "places": storage.count("Place"), 
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"), 
+        "users": storage.count("user") 
+    }
+    
+    return jsonify(stats)
+
