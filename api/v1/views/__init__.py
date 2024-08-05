b7f8a0a45f859e3afa03e9dd65d6390a6132716e
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-08-06 00:39:23 +0100

    new

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,4 +4,5 @@ from flask import Blueprint
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 # Wildcard import everything from the package api.v1.views.index
+
 from api.v1.views.index import *
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,6 +5,7 @@ import statement
 from api.v1.views import app_views
 from flask import jsonify
 
+
 @app_views.route('/status', methods=['GET'])
 def status():
     """
